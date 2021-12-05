000d4e5faedb501222e5ab1799eb73cae2dbfa56
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-05 17:43:45 +0900

    Nested describes and context method

---
+++ rspec-course/spec/context_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe '#even? method' do
+    # it 'should return true if number is even'
+    # using nested describes adds more organization
+
+    context 'with even number' do
+        it 'should return true' do
+            expect(4.even?).to eq(true)
+        end 
+    end 
+
+    context 'with odd number' do
+        it 'should return false' do
+            expect(5.even?).to eq(false)
+        end 
+    end 
+end 

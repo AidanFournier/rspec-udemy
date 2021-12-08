33bb196bb43dd216a151ed50896a9c9abc1b2bcc
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-08 21:35:45 +0900

    Practiced using subject method

--- rspec-course/spec/implicit_subject_spec.rb
+++ rspec-course/spec/implicit_subject_spec.rb
@@ -2,10 +2,17 @@ RSpec.describe Hash do
     # let(:my_hash) { {} }
     # le(:subject) { Hash.new }
     # subject == Hash.new
+    # will be same instance within an example, but a new one between different examples
 
     it 'should start off empty' do
-        puts subject
-        puts subject.class
+        # puts subject
+        # puts subject.class
+        expect(subject.length).to eq(0)
+        subject[:some_key] = "Some value"
+        expect(subject.length).to eq(1)
+    end
+
+    it 'is isolated between examples' do
         expect(subject.length).to eq(0)
     end
 

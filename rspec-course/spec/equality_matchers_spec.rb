0729b9e1ebdbbf357a44f2516f2f20231493de39
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-09 22:27:19 +0900

    Using equal/be matcher

--- rspec-course/spec/equality_matchers_spec.rb
+++ rspec-course/spec/equality_matchers_spec.rb
@@ -21,4 +21,29 @@ RSpec.describe 'equality matchers' do
         end
     end
 
+    describe 'equal and be matcher' do
+        # difference between equality (ex. two houses that look/function the same) and identity (ex. two totally distinct houses that have same floor plan, address)
+        # identity == the same object in computer memory
+        # equality == the two things look the same
+        # think of variable as the address to a house, and the thing it's pointing to as the house
+        #, below, [1, 2, 3] is the house, c is the address
+        # c and d are equal houses, but not identical 
+        # c and e are different addresses pointing to the same house (therefore equal)
+        let(:c) { [1, 2, 3] }
+        let(:d) { [1, 2, 3] }
+        let(:e) { c }
+
+        it 'cares about object identity' do
+            expect(c).to eq(d)
+            expect(c).to eql(d)
+
+            expect(c).to equal(e)
+            # expect(c).to equal(d)
+            expect(c).to be(e)
+
+            expect(c).not_to equal(d)
+            expect(c).not_to equal([1, 2, 3]) # this is a new array, new object, so not the same as c
+        end
+    end
+
 end

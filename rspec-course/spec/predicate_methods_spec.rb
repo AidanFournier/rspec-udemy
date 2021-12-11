9a611b52b62b81d8a57c884acd49d75b85d6316f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-11 15:28:57 +0900

    Predicate methods and matchers

---
+++ rspec-course/spec/predicate_methods_spec.rb
@@ -0,0 +1,20 @@
+# a predicate method is just a method that returns a boolean value, true or false
+# they all end with a ?
+
+# puts 0.zero?
+# puts 15.zero?
+
+# puts 2.even?
+# puts 3.even?
+
+# puts 11.odd?
+# puts 16.odd?
+
+# puts [].empty?
+# puts [1].empty?
+
+RSpec.describe 'predicate methods and predicate matchers' do
+    it 'can be tested with Ruby methods' do
+        result = 16.
+    end
+end

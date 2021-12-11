d989cf5072550a449de82375cb68e1caf8f2fc7f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-11 16:16:08 +0900

    Practiced using shorthand predicate matchers

--- rspec-course/spec/predicate_methods_spec.rb
+++ rspec-course/spec/predicate_methods_spec.rb
@@ -15,6 +15,21 @@
 
 RSpec.describe 'predicate methods and predicate matchers' do
     it 'can be tested with Ruby methods' do
-        result = 16.
+        result = 16 / 2
+        expect(result.even?).to eq(true)
+        #^ this is technically fine, but because it's such a common use RSpec gives us a shortcut
+    end
+
+    it 'can be tested with predicate matchers' do
+        expect(16 / 2).to be_even
+        expect(16 / 3).not_to be_even
+
+        expect(15).to be_odd
+        expect(0).to be_zero
+        expect([]).to be_empty
+    end
+
+    describe 0 do
+        it { is_expected.to be_zero }
     end
 end

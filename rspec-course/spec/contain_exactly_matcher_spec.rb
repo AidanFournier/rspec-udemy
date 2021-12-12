6a8bc43d9329d48af423450a566625e98b4d7f8f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-12 16:52:30 +0900

    start_with and end_with matchers

--- rspec-course/spec/contain_exactly_matcher_spec.rb
+++ rspec-course/spec/contain_exactly_matcher_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'contain_exactly matcher' do
     subject { [1, 2, 3] }
 
     describe 'long form syntax' do
-        it 'should check for the preesence of all elements' do
+        it 'should check for the presence of all elements' do
             expect(subject).to contain_exactly(1, 2, 3)
             expect(subject).to contain_exactly(3, 2, 1)
             expect(subject).to contain_exactly(2, 3, 1)

1f07788eef3e58ab73a3ffb53a7d3542121a3722
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-12-12 16:28:16 +0900

    Using contain_exactly method

---
+++ rspec-course/spec/contain_exactly_matcher_spec.rb
@@ -0,0 +1,21 @@
+# this method doesn't care about the order of the elements, just checking to see that they're present in an array
+# if there are extra elements in an array, the test will fail, and if there are not enough elements, the test will fail
+
+RSpec.describe 'contain_exactly matcher' do
+    subject { [1, 2, 3] }
+
+    describe 'long form syntax' do
+        it 'should check for the preesence of all elements' do
+            expect(subject).to contain_exactly(1, 2, 3)
+            expect(subject).to contain_exactly(3, 2, 1)
+            expect(subject).to contain_exactly(2, 3, 1)
+
+            # expect(subject).to contain_exactly(1, 2)
+            # expect(subject).to contain_exactly(1, 2, 3, 4)
+        end
+    end
+
+    it { is_expected.to contain_exactly(1, 2, 3) }
+    it { is_expected.to contain_exactly(3, 1, 2) }
+
+end
